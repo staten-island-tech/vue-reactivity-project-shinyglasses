55da8fc0f4324a1f41f01f06bc8452e52d18a37a
Crystal-Lin1 <crunchytvtable@gmail.com>
2026-02-11 12:31:50 -0500

    feat: goal creation form (wip)

--- app/src/components/GoalCreation.vue
+++ app/src/components/GoalCreation.vue
@@ -1,11 +1,39 @@
 <template>
     <div>
-
+        <h1>Goal Creation</h1>
+        <form action="submit" @submit.prevent="addGoal(goal)">
+            <input type="text" placeholder="Goal Title" v-model="goal.name"/>
+            <h2>Status:</h2>
+            <select v-model="goal.status">
+                <option value="not-started">Not Started</option>
+                <option value="in-progress">In Progress</option>
+                <option value="completed">Completed</option>
+            </select>
+            <h2>Timeline:</h2>
+            <select v-model="goal.timeline">
+                <option value="long-term">Long Term</option>
+                <option value="medium-term">Medium Term</option>
+                <option value="short-term">Short Term</option>
+            </select>
+            <button type="submit">Create Goal</button>
+        </form>
     </div>
 </template>
 
 <script>
-    
+    import GoalCard from './GoalCard.vue';
+    import { ref } from 'vue';
+    const goals = ref([
+
+    ]);
+    function addGoal(goal) {
+        goals.value.push(goal);
+    }
+    export default {
+        components: {
+            GoalCard
+        }
+    }
 </script>
 
 <style scoped>
--- app/src/views/GoalPage.vue
+++ app/src/views/GoalPage.vue
@@ -1,11 +1,16 @@
 <template>
-
-    <GoalCard> </GoalCard>
+    <h1>Goal Tracker</h1>
+    <button @click="<GoalCreation></GoalCreation>">Create Goal</button>
+    <div>
+        <!--put goals here-->
+    </div>
 </template>
 
 <script setup>
 import GoalCard from '../components/GoalCard.vue'
 import { ref } from 'vue';
+import GoalCreation from '../components/GoalCreation.vue';
+
 const goals = ref([
 
 ]) //array of objs
